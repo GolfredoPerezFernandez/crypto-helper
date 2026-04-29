414c78f63ce09328c94664e4e90bc2269e0f9183
Golfredo Perez Fernandez <36853339+GolfredoPerezFernandez@users.noreply.github.com>
2026-04-29 16:25:52 -0400

    test

--- src/routes/[locale]/smart-signals/index.tsx
+++ src/routes/[locale]/smart-signals/index.tsx
@@ -1,2 +1,5 @@
-export * from "../dashboard/smart-signals/index";
-export { default } from "../dashboard/smart-signals/index";
+import type { RequestHandler } from "@builder.io/qwik-city";
+
+export const onGet: RequestHandler = async ({ params, redirect }) => {
+  throw redirect(308, `/${params.locale ?? "en-us"}/dashboard/smart-signals/`);
+};
